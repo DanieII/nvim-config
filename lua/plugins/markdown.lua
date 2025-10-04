1df851130a10b91a88e299fac5f198981ff9fa2e
Daniel Bogdanov <danielbogdanov1748@gmail.com>
2025-10-04 14:56:44 +0300

    Configure markdown rendering

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,4 +1,13 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        file_types = { "markdown", "copilot-chat" },
+        heading = {
+            position = "inline",
+        },
+        code = { position = "right", width = "block", left_pad = 2, right_pad = 4, border = "thick" },
+    },
 }
